bbff0d8f520f68c801124215f2fa80b816086554
Dung8229 <22028229@cnu.edu.vn>
2023-03-02 22:07:42 +0700

    'init'

--- BaseObject.cpp
+++ BaseObject.cpp
@@ -2,7 +2,6 @@
 
 BaseObject::BaseObject()
 {
-	//Initialize
 	mTexture = NULL;
 	mWidth = 0;
 	mHeight = 0;
@@ -11,19 +10,15 @@ BaseObject::BaseObject()
 
 BaseObject::~BaseObject()
 {
-	//Deallocate
 	free();
 }
 
 bool BaseObject::loadFromFile(SDL_Renderer* gRenderer, std::string path)
 {
-	//Get rid of preexisting texture
 	free();
 
-	//The final texture
 	SDL_Texture* newTexture = NULL;
 
-	//Load image at specified path
 	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
 	if( loadedSurface == NULL )
 	{
@@ -31,10 +26,8 @@ bool BaseObject::loadFromFile(SDL_Renderer* gRenderer, std::string path)
 	}
 	else
 	{
-		//Color key image
 		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0, 0 ) );
 
-		//Create texture from surface pixels
         newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
 		if( newTexture == NULL )
 		{
@@ -42,23 +35,19 @@ bool BaseObject::loadFromFile(SDL_Renderer* gRenderer, std::string path)
 		}
 		else
 		{
-			//Get image dimensions
 			mWidth = loadedSurface->w;
 			mHeight = loadedSurface->h;
 		}
 
-		//Get rid of old loaded surface
 		SDL_FreeSurface( loadedSurface );
 	}
 
-	//Return success
 	mTexture = newTexture;
 	return mTexture != NULL;
 }
 
 void BaseObject::free()
 {
-	//Free texture if it exists
 	if( mTexture != NULL )
 	{
 		SDL_DestroyTexture( mTexture );
@@ -70,35 +59,29 @@ void BaseObject::free()
 
 void BaseObject::setColor( Uint8 red, Uint8 green, Uint8 blue )
 {
-	//Modulate texture rgb
 	SDL_SetTextureColorMod( mTexture, red, green, blue );
 }
 
 void BaseObject::setBlendMode( SDL_BlendMode blending )
 {
-	//Set blending function
 	SDL_SetTextureBlendMode( mTexture, blending );
 }
 
 void BaseObject::setAlpha( Uint8 alpha )
 {
-	//Modulate texture alpha
 	SDL_SetTextureAlphaMod( mTexture, alpha );
 }
 
 void BaseObject::render(SDL_Renderer* gRenderer, int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
 {
-	//Set rendering space and render to screen
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight};
 
-	//Set clip rendering dimensions
 	if( clip != NULL )
 	{
 		renderQuad.w = clip->w;
 		renderQuad.h = clip->h;
 	}
 
-	//Render to screen
 	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
 }
 
